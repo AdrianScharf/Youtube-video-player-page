3afd2d5ee19c24a3b8d6ffbd629a7b18f428393d
AdrianScharf <adrian.jrez99@gmail.com>
2023-05-05 16:14:59 -0600

    last changes, menu and side menu ready, main sections already positioned, just missing video, suggestions images and comments

---
+++ menu.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const menuItems = document.querySelector('.menu-items');
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    menuItems.classList.toggle('show');
+})
