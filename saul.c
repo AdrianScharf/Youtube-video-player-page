64ee86c5bfd2577da8ada3a7aad8b63c5dcac8ca
AdrianScharf <adrian.jrez99@gmail.com>
2023-05-06 18:05:59 -0600

    remaining changes for the youtube video player page

---
+++ saul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, r;
+    prinf("dame el primer numero");
+    scanf("%i", &a);
+    printf("dame el segundo numero");
+    sacnf("%i", &b);
+    r=a*b;
+    printf("el resultado es;");
+    return 0;
+
+}
